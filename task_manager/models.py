f97cc5f8f0f78ace380c87269ee50661ac89b83c
viktor-77 <vumanskiy0664041341@gmail.com>
2024-11-16 22:31:55 +0200

    add TaskType model with unique name field and default ordering

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "Тип завдання"
+        verbose_name_plural = "Типи завдань"
+
+    def __str__(self) -> str:
+        return self.name
