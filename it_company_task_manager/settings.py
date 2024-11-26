45ddaffcec3833a5c19305f0b5258f1fced3d79d
viktor-77 <vumanskiy0664041341@gmail.com>
2024-11-26 23:26:38 +0200

    refactor 2 errors

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(x6+qbryl(wyes9*(3j11#fm+o!1rdj@b9p!k75xeue#6wr*d5"
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 INTERNAL_IPS = [
 	"127.0.0.1",
