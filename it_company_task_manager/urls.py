e68a78f47802330983d59c6a8074108a8b6214a3
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-01 23:08:04 +0200

    refactor urls.py

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+
+if settings.DEBUG:
+	urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
