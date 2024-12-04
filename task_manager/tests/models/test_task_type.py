4b38dbb45d642dd004ca4ad221ba0b1e96fa313e
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-05 00:38:47 +0200

    refactor test model

--- task_manager/tests/models/test_task_type.py
+++ task_manager/tests/models/test_task_type.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from task_manager.models import TaskType
+from task_manager.tests.models.utils import get_name_over_limit
 
 
 class TaskTypeTestCase(TestCase):
@@ -9,15 +10,26 @@ class TaskTypeTestCase(TestCase):
 	def setUp(self):
 		self.task_type = TaskType.objects.create(name="Bug")
 	
-	def test_name_max_length(self):
-		long_name = "x" * 101
+	def test_name_max_length_validation_on_create(self):
+		with self.assertRaises(ValidationError):
+			TaskType.objects.create(name=get_name_over_limit())
+	
+	def test_name_max_length_validation_on_update(self):
+		self.task_type.name = get_name_over_limit()
 		
 		with self.assertRaises(ValidationError):
-			TaskType.objects.create(name=long_name)
+			self.task_type.save()
 	
-	def test_unique_name(self):
+	def test_unique_name_validation_on_create(self):
 		with self.assertRaises(ValidationError):
 			TaskType.objects.create(name=self.task_type.name)
+	
+	def test_unique_name_validation_on_update(self):
+		test_task_type = TaskType.objects.create(name="Refactoring")
+		test_task_type.name = self.task_type.name
+		
+		with self.assertRaises(ValidationError):
+			test_task_type.save()
 	
 	def test_ascending_ordering(self):
 		TaskType.objects.create(name="Refactoring")
