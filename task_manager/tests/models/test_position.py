5d5b4bce8d77ae6a4a309ff2a9b035345bb6a238
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-05 00:39:16 +0200

    refactor test model

--- task_manager/tests/models/test_position.py
+++ task_manager/tests/models/test_position.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from task_manager.models import Position
+from task_manager.tests.models.utils import get_name_over_limit
 
 
 class PositionTestCase(TestCase):
@@ -9,15 +10,26 @@ class PositionTestCase(TestCase):
 	def setUp(self):
 		self.position = Position.objects.create(name="Developer")
 	
-	def test_name_max_length(self):
-		long_name = "x" * 101
+	def test_name_max_length_on_update(self):
+		self.position.name = get_name_over_limit()
 		
 		with self.assertRaises(ValidationError):
-			Position.objects.create(name=long_name)
+			self.position.save()
 	
-	def test_unique_name(self):
+	def test_name_max_length_on_create(self):
+		with self.assertRaises(ValidationError):
+			Position.objects.create(name=get_name_over_limit())
+	
+	def test_unique_name_validation_on_create(self):
 		with self.assertRaises(ValidationError):
 			Position.objects.create(name=self.position.name)
+	
+	def test_unique_name_validation_on_update(self):
+		test_position = Position.objects.create(name="Manager")
+		test_position.name = self.position.name
+		
+		with self.assertRaises(ValidationError):
+			test_position.save()
 	
 	def test_ascending_ordering(self):
 		Position.objects.create(name="Manager")
