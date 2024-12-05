6b4e83a22ef305f0bbceaefde87fee745cca677b
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-05 04:14:17 +0200

    refactor test model

--- task_manager/tests/models/utils.py
+++ task_manager/tests/models/utils.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+
 from task_manager.models import Position, TaskType
 
 
@@ -11,3 +14,9 @@ def create_position(name: str) -> Position:
 
 def create_task_type(name: str) -> Position:
 	return TaskType.objects.create(name=name)
+
+
+def create_user() -> AbstractUser:
+	return get_user_model().objects.create_user(
+		username="test-user", password="test-password",
+	)
